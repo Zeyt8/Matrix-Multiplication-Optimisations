2bd794840035dbfffb9c82e34a23b062574bc46d
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2023-05-02 19:57:52 +0300

    Add blas implementation

--- 2-matrix/skel/solver_blas.c
+++ 2-matrix/skel/solver_blas.c
@@ -2,6 +2,7 @@
  * Tema 2 ASC
  * 2023 Spring
  */
+#include <cblas.h>
 #include "utils.h"
 
 /* 
@@ -9,5 +10,14 @@
  */
 double* my_solver(int N, double *A, double *B) {
 	printf("BLAS SOLVER\n");
-	return NULL;
+	double *Result = (double *) calloc(N * N, sizeof(double));
+	// Result = B
+	cblas_dcopy(N * N, B, 1, Result, 1);
+	// Result = A * B
+	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1.0, A, N, Result, N);
+	// Result = Result * A_t
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1.0, A, N, Result, N);
+	// Result += B_t * B_t
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasTrans, N, N, N, 1.0, B, N, B, N, 1.0, Result, N);
+	return Result;
 }
